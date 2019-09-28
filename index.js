eb27bb96434174eacca5cea754312d22c258ea4f
unknown <rmtestard@gmail.com>
2019-09-28 16:54:33 +0200

    controllers implementation

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("./util/path");
+// const rootDir = require('./util/path');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const errorController = require('./controllers/404');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes.router);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  console.log("I always run");
+app.use('/', (req, res, next) => {
+  console.log('I always run');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
